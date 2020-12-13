458fd57a88693c9d0cde34f13342dec6d53d9257
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-12-13 17:14:59 +0100

    Added autoreddit.js

---
+++ autoReddit.js
@@ -0,0 +1,32 @@
+const Discord = require('discord.js');
+const Client = new Discord.Client();
+
+const { botName, botID, prefix, token, redditToken, subredditName, nswfAllowed, redditChannel, AdminRole, endMessage } = require('./config.json');
+
+
+Client.once('ready', () =>  {
+
+    setInterval(nsfw, 1000);
+
+});
+
+function nsfw() {
+    var snoowrap = require('snoowrap');
+
+    const r = new snoowrap({
+        userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
+        clientId: 'wxF4146B4858Sg',
+        clientSecret: redditToken,
+        username: 'Sea-Box5852',
+        password: 'Sea-Box5852'
+    });
+
+    post = r.getRandomSubmission(subredditName).then((post) => {
+        var link = post.url;
+
+        console.log(link);
+        Client.channels.cache.get(redditChannel).send(link);
+    });
+}
+
+Client.login('Nzc0OTc0ODQxMjk5OTI3MTAx.X6flkA.qvRYvQC-qXbc90cSlIbNsYR01nE');

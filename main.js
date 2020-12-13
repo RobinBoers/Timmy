9e5b4a2179bd4ffe0ce2029c122816025a92855d
Robin Boers <60298132+RobinBoers@users.noreply.github.com>
2020-12-13 15:51:15 +0100

    Added reddit command

--- main.js
+++ main.js
@@ -3,71 +3,94 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // get tokens, prefix and other settings
-const { botName, prefix, token, redditToken, nswfAllowed, AdminRole } = require('./config.json');
+const { botName, botID, prefix, token, redditToken, subredditName, nswfAllowed, AdminRole, endMessage } = require('./config.json');
 
 
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
-    console.log('Fabby is Ready to Roll!');
+    console.log('Fabby is Ready to Roll!'+endMessage);
     // message.channel.send('Boop.');
 });
 
 // listen for messages
 client.on('message', message => {
-    console.log(message.author.username+": "+message.content);
-    msg = message.content.toLowerCase();
+    if(message.author.bot) return;
+    console.log(message.author.username+": "+message.content+endMessage);
+    // msg = message.content.toLowerCase();
+    msg = message.content;
     // console.log("Lowercase message: "+ msg);
+
+    // Set correct nickname
+    if (message.guild.members.cache.get(botID).hasPermission("MANAGE_NICKNAMES") && message.guild.members.cache.get(botID).hasPermission("CHANGE_NICKNAME")) {
+        message.guild.members.cache.get(botID).setNickname(botName);
+    } else {
+        message.channel.send("I dont have the permissons to change my nickname in this server.");
+    }
 
     if (msg === prefix + `ping`) {
         var reply = "Pong.";
-        console.log(botName+": "+reply);
+        console.log(botName+": "+reply+endMessage);
         message.channel.send(reply);
     } 
     else if(msg === prefix + `beep`) {
 
         var reply = "Boop.";
-        console.log(botName+": "+reply);
+        console.log(botName+": "+reply+endMessage);
         message.channel.send(reply);
 
     }
-    else if (msg === prefix + `stats`) {
+    else if (msg === prefix + `serverinfo` || msg === prefix + `serverstats`) {
 
-        var reply = `
-        Server name: ${message.guild.name}\n
-        Total members: ${message.guild.memberCount}
-        `;
-        console.log(botName+": "+reply);
+        var reply = `Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`;
+        console.log(botName+": "+reply+endMessage);
         message.channel.send(reply);
 
     }
-    else if (msg === prefix + `user-info`) {
+    else if (msg === prefix + `userinfo`) {
 
-        var reply = `
-        Your avatar:\n
-        <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>\n
-        Your username: ${message.author.username}\n
-        Your ID: ${message.author.id}
-        `;
-        console.log(botName+": "+reply);
+        var reply = `Your avatar:\n<${message.author.displayAvatarURL({ format: "png", dynamic: true })}>\nYour username: ${message.author.username}\nYour ID: ${message.author.id}`;
+        console.log(botName+": "+reply+endMessage);
         message.channel.send(reply);
 
     }
     else if (msg === prefix + `clear`) {
 
-        console.log("Deleting 100 messages.");
+        console.log("Deleting 100 messages."+endMessage);
 
         message.channel.bulkDelete(100, true).catch(err => {
             console.error(err);
             var reply = "There was an error trying to delete messages in this channel!";
-            console.log(botName+": "+reply);
+            console.log(botName+": "+reply+endMessage);
             message.channel.send(reply);
         });
 
     }
-    
-    
-    
+    else if (msg === prefix + `getReddit`) {
+
+        // Using snoowrap to access reddit API
+        var snoowrap = require('snoowrap');
+
+            // Snoowrap config, logging into a burner account with username + password
+            const r = new snoowrap({
+                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:82.0) Gecko/20100101 Firefox/82.0',
+                clientId: 'wxF4146B4858Sg',
+                clientSecret: redditToken,
+                username: 'Sea-Box5852',
+                password: 'Sea-Box5852'
+            });
+
+            // Get random submission (=post) from the subreddit configured in config.json
+            post = r.getRandomSubmission(subredditName).then((post) => {
+
+                var link = post.url;
+
+                console.log(botName+": "+link+endMessage);
+                message.channel.send(link);
+
+            });
+
+    }
     
 });
 
